d7f3f286c1d874e77764d58d265b202556a3102a
Sezer <iletisimyuceszr@gmail.com>
2026-07-03 19:39:40 +0300

    arka plan salfom oldu

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSe8bmnxW
 
 // İstersen kendi arka plan görselinin linkini buraya yapıştır (örn. imgur direkt görsel linki).
 // Boş bırakırsan otomatik olarak dokulu bir fantasy arka plan deseni kullanılır.
-const BACKGROUND_IMAGE_URL = 'https://i.imgur.com/wHml5RH.jpg';
+const BACKGROUND_IMAGE_URL = 'https://imgur.com/nwLezlA';
 
 type Character = {
   name: string;
